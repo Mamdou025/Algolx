ce7ff946d68f996968bd252228343b5f60041f6a
Mamdou025 <60107396+Mamdou025@users.noreply.github.com>
2023-02-10 17:14:55 -0500

    Create plugininit.js

---
+++ plugininit.js
@@ -0,0 +1,29 @@
+  (function() {
+    // INITIALIZATION OF MEGA MENU
+    // =======================================================
+    const megaMenu = new HSMegaMenu('.js-mega-menu', {
+      desktop: {
+        position: 'left'
+      }
+    })
+
+
+    // INITIALIZATION OF SHOW ANIMATIONS
+    // =======================================================
+    new HSShowAnimation('.js-animation-link')
+
+
+    // INITIALIZATION OF BOOTSTRAP VALIDATION
+    // =======================================================
+    HSBsValidation.init('.js-validate', {
+      onSubmit: data => {
+        data.event.preventDefault()
+        alert('Submited')
+      }
+    })
+
+
+    // INITIALIZATION OF HEADER
+    // =======================================================
+    new HSHeader('#header').init()
+  })()

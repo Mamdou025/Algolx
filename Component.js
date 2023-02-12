e68a084be4ef4f8fa9d994556a51a0c72a599d65
Mamdou025 <60107396+Mamdou025@users.noreply.github.com>
2023-02-11 21:57:50 -0500

    Create Component.js

---
+++ Component.js
@@ -0,0 +1,14 @@
+const Component =()=>{
+    return (
+        <div>
+        <h1>
+        Hello World
+        </h1>
+            
+        
+        </div>
+      )
+
+
+}
+ReactDOM.render(<Component />, document.getElementById("reactcomponent"));
